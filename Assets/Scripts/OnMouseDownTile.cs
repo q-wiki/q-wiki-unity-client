c9d98164cefb7526f16d03c3c05f67bcc716660a
Antonia Berger <AntoniaBerger@gmx.net>
2019-05-15 23:53:16 +0200

    added some ui elements

---
+++ Assets/Scripts/OnMouseDownTile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseDownTile : MonoBehaviour
+{
+
+    Renderer objectRenderer;
+
+    public Transform prefab;
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+    }
+
+
+
+
+
+    void OnMouseDown()
+    {
+        //Debug.Log(meshRenderer.material.color);
+
+        if (objectRenderer.sharedMaterial.name == "Red")
+        {
+            Debug.Log("Red");
+        }
+        else if (objectRenderer.sharedMaterial.name == "Blue")
+        {
+            Debug.Log("Blue");
+        }
+        else if (objectRenderer.sharedMaterial.name == "White")
+        {
+            Debug.Log("White");
+            Instantiate(prefab);
+        } else
+        {
+            Debug.Log("Invalid");
+        }
+    }
+}

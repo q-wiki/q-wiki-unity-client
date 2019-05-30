da9d0d8b8b8c6530407617a95beafcfe6e154f5f
davidmllr <david.mllr@web.de>
2019-05-30 12:31:18 +0200

    communicator basic structure

--- Assets/Scripts/Communicator.cs
+++ Assets/Scripts/Communicator.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-namespace Application
+using UnityEngine.Networking;
+
+public class Communicator
 {
-    public class Communicator
+
+    UnityWebRequest webRequest = new UnityWebRequest();
+
+    // TODO: connect to REST API to really "communicate"
+
+
+    public Communicator()
     {
-        public Communicator()
-        {
-        }
+
+    }
+
+    public string GetMinigame()
+    {
+        // TODO: GET-REQUEST an Backend schicken
+        // return JsonUtility.FromJson<Minigame>(json);
+
+        return null;
+
+    }
+
+    public bool checkAnswer()
+    {
+        // TODO: Post-Request an Backend, um MinigameResult zu erhalten
+        return false;
     }
 }

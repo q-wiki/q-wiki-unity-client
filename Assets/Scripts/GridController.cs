9295d1d2c990755dfa53fa203cb6881dc6464ad3
Jenni <eisundsahne@web.de>
2019-06-19 18:40:41 +0200

    Added TileScript to handle individual tiles, including categories

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using WikidataGame;
@@ -6,10 +7,12 @@ using WikidataGame.Models;
 
 public class GridController: MonoBehaviour
 {
-    public GameObject[] hexPrefabFree;
-    public GameObject[] hexPrefabOcc;
-    public GameObject[] hexPrefabSpawn;
+    public GameObject[] hexPrefabFree0;
+    public GameObject[] hexPrefabFree1;
+    public GameObject[] hexPrefabFree2;
+    public GameObject hexPrefabSpawn;
 
+    public bool addGap = true;
     public float gap = 0.0f;
 
     private float hexWidth = 1.732f;
@@ -21,19 +24,27 @@ public class GridController: MonoBehaviour
 
     Vector3 startPos;
 
-    public void GenerateGrid(IList<IList<Tile>> tiles)
+    public void Start()
     {
-        tileSystem = tiles;
-        tileArray = new GameObject[tileSystem.Count, tileSystem[0].Count];
-        AddGap();
-        CalcStartPos();
-        CreateGrid();
     }
 
-    void AddGap()
+    public void GenerateGrid(IList<IList<Tile>> tiles)
     {
-        hexWidth += hexWidth * gap;
-        hexHeight += hexHeight * gap;
+        // Tiles from Backend
+        tileSystem = tiles;
+        // Gameobject Tile array with right length and depth
+        tileArray = new GameObject[tileSystem.Count, tileSystem[0].Count];
+       
+        if (addGap)
+        {
+            AddGap();
+        }
+
+        // Calculate the starting position for grid
+        CalcStartPos();
+
+        CreateGrid();
+        
     }
 
     void CreateGrid()
@@ -42,32 +53,58 @@ public class GridController: MonoBehaviour
         {
             for (int x = 0; x< tileSystem.Count; x++)
             {
-                int randomIndexFree = Random.Range(0, hexPrefabFree.Length);
-                int randomIndexOcc= Random.Range(0, hexPrefabOcc.Length);
+                
                 GameObject tile;
-                float height = 0;
         
                 if (tileSystem[x][z] != null)
                 {
+                    // Choose random tile design for right level
+                    int randomIndexFree;
+                    GameObject[] hexPrefabFree;
+                    if (tileSystem[x][z].Difficulty == 0)
+                    {
+                        randomIndexFree = UnityEngine.Random.Range(0, hexPrefabFree0.Length);
+                        hexPrefabFree = hexPrefabFree0;
+                    } else if (tileSystem[x][z].Difficulty == 1)
+                    {
+                        randomIndexFree = UnityEngine.Random.Range(0, hexPrefabFree1.Length);
+                        hexPrefabFree = hexPrefabFree1;
+                    }
+                    else if (tileSystem[x][z].Difficulty == 2)
+                    {
+                        randomIndexFree = UnityEngine.Random.Range(0, hexPrefabFree2.Length);
+                        hexPrefabFree = hexPrefabFree2;
+                    }
+                    else {
+                        Debug.Log("ERROR WRONG LEVEL");
+                        randomIndexFree = UnityEngine.Random.Range(0, hexPrefabFree0.Length);
+                        hexPrefabFree = hexPrefabFree0;
+                    }
 
                     tile = Instantiate(hexPrefabFree[randomIndexFree]) as GameObject;
-                    //tile = Instantiate(tilePrefab) as GameObject;
-                    height = (float)tileSystem[x][z].Difficulty;
+
+                    tile.AddComponent<TileController>();
+                    tile.GetComponent<TileController>().availableCategories = tileSystem[x][z].AvailableCategories;
+                    tile.GetComponent<TileController>().id = tileSystem[x][z].Id;
+                    Debug.Log(tileSystem[x][z].OwnerId+ "Test"+ tileSystem[x][z].ChosenCategoryId);
+
+
+                    float height = (float)tileSystem[x][z].Difficulty;
+
+                    Vector3 gridPos = new Vector3(x, 0, z);
+
+                    tile.transform.position = CalcWorldPos(gridPos);
+                    tile.transform.parent = this.transform;
+                    tile.name = "GridTile" + x + "|" + z;
+
+                    tileArray[x, z] = tile;
                 }
-                else
-                {
-                    tile = Instantiate(hexPrefabOcc[randomIndexOcc]) as GameObject;
-                }
+                                                
 
-                tileArray[x, z] = tile;                                                  
 
-                Vector3 gridPos = new Vector3(x, height, z);
-
-                tile.transform.position = CalcWorldPos(gridPos);
-                tile.transform.parent = this.transform;
-                tile.name = "GridTile" + x + "|" + z;
             }
         }
+
     }
 
     Vector3 CalcWorldPos(Vector3 gridPos)
@@ -79,7 +116,6 @@ public class GridController: MonoBehaviour
 
         float z = startPos.x + gridPos.x * hexWidth + offset;
         float x = startPos.z - gridPos.z * hexHeight * 0.75f;
-        
         return new Vector3(x, y, z);
     }
 
@@ -94,5 +130,11 @@ public class GridController: MonoBehaviour
 
         startPos = new Vector3(x, 0, z);
     }
-    
+
+
+    void AddGap()
+    {
+        hexWidth += hexWidth * gap;
+        hexHeight += hexHeight * gap;
+    }
 }
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WikidataGame;
+using WikidataGame.Models;
 
 public class TileController : MonoBehaviour
 {
+    public string id;
+    public string ownerId;
 
+    public int difficulty;
+    public IList<WikidataGame.Models.Category> availableCategories;
+    public WikidataGame.Models.Category chosenCategories;
 
     void Start()
     {
-
-        
     }
+
     void Update()
     {
         

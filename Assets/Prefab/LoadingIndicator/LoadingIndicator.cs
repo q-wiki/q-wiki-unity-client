f4932fb3afcaf93a85c37244428a304ca48de6ba
davidmllr <david.mllr@web.de>
2019-07-03 17:03:53 +0200

    created simple loading indicator
    simple loading indicator logic
    can be triggered with show() and hide()

---
+++ Assets/Prefab/LoadingIndicator/LoadingIndicator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingIndicator : MonoBehaviour
+{
+
+    /**
+     * private fields
+     */
+    private CanvasGroup _canvasGroup => GetComponentInChildren<CanvasGroup>();
+    private Animator _animator => GetComponentInChildren<Animator>();
+    
+    /**
+     * static fields
+     */
+
+    public static LoadingIndicator Instance;
+
+    public void Awake()
+    { 
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("LoadingIndicator");
+        if(objs.Length > 1)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        
+        Instance = this;
+        Hide();
+    }
+
+    public void Show()
+    {
+        _canvasGroup.alpha = 1;
+        _canvasGroup.blocksRaycasts = true;
+        _animator.enabled = true;
+    }
+
+    public void Hide()
+    {
+        _canvasGroup.alpha = 0;
+        _canvasGroup.blocksRaycasts = false;
+        _animator.enabled = false;
+    }
+}

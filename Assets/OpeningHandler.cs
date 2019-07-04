99e9d06aaf6af80fed8b9fd0bfeff7d247c98909
davidmllr <david.mllr@web.de>
2019-07-04 09:09:08 +0200

    change some menu things

---
+++ Assets/OpeningHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        await Task.Delay(5000);
+        LoadingIndicator.Instance.Hide();
+        SceneManager.LoadSceneAsync("Menu");
+    }
+
+}

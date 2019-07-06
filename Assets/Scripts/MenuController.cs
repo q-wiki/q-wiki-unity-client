e8d6acaf03ddab07a80fe843da3f71aac9afd226
davidmllr <david.mllr@web.de>
2019-07-06 12:18:14 +0200

    some changes

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -323,36 +323,42 @@ public class MenuController : MonoBehaviour
         categoryCanvas.SetActive(false);
     }
 
-
-    public void ShowCategoryPanel()
+    public void LevelUpOrAttackTile()
     {
         var chosenCategoryId = selectedTile.GetComponent<TileController>().chosenCategoryId;
-
-        if (!String.IsNullOrEmpty(chosenCategoryId))
+        if (chosenCategoryId == null)
         {
-            // Someone captured this tile already
-            StartMiniGame(chosenCategoryId);
+            // we're on our start tile
+            ShowCategoryPanel();
         }
         else
         {
-            // We're trying to capture it for the first time
             actionPanel.SetActive(false);
-            categoryPanel.SetActive(true);
-
-            var availableCategories = selectedTile.GetComponent<TileController>().availableCategories;
-
-            c1.GetComponentInChildren<Text>().text = availableCategories[0].Title;
-            c2.GetComponentInChildren<Text>().text = availableCategories[1].Title;
-            c3.GetComponentInChildren<Text>().text = availableCategories[2].Title;
-
-            c1.onClick.RemoveAllListeners();
-            c2.onClick.RemoveAllListeners();
-            c3.onClick.RemoveAllListeners();
-
-            c1.onClick.AddListener(() => { StartMiniGame(availableCategories[0].Id); });
-            c2.onClick.AddListener(() => { StartMiniGame(availableCategories[1].Id); });
-            c3.onClick.AddListener(() => { StartMiniGame(availableCategories[2].Id); });
+            StartMiniGame(chosenCategoryId);
         }
+    }
+
+
+    public void ShowCategoryPanel()
+    {
+        // We're trying to capture it for the first time
+        actionPanel.SetActive(false);
+        categoryPanel.SetActive(true);
+
+        var availableCategories = selectedTile.GetComponent<TileController>().availableCategories;
+
+        c1.GetComponentInChildren<Text>().text = availableCategories[0].Title;
+        c2.GetComponentInChildren<Text>().text = availableCategories[1].Title;
+        c3.GetComponentInChildren<Text>().text = availableCategories[2].Title;
+
+        c1.onClick.RemoveAllListeners();
+        c2.onClick.RemoveAllListeners();
+        c3.onClick.RemoveAllListeners();
+
+        c1.onClick.AddListener(() => { StartMiniGame(availableCategories[0].Id); });
+        c2.onClick.AddListener(() => { StartMiniGame(availableCategories[1].Id); });
+        c3.onClick.AddListener(() => { StartMiniGame(availableCategories[2].Id); });
+        
     }
 
     public void CloseCategoryAndActionPamnel()

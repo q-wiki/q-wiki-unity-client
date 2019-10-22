d241776976761d7d5805b6b90b629c6f05da26be
golemmoja <s0540608@htw-berlin.de>
2019-10-22 14:18:28 +0200

    google signin, delete deprecated stuff?

---
+++ Assets/Scripts/SignInController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class SignInController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SignIn()
+    {
+        // authenticate user:
+        Social.localUser.Authenticate((bool success) => {
+            Debug.log(success);
+            // handle success or failure
+        });
+
+    }
+}
